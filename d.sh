559a9e4be5c6d03360e5b9385df80f7e0313157d
foxscallion <foxscallion@163.com>
2022-01-09 22:16:48 +0800

    win git commit blog

--- d.sh
+++ d.sh
@@ -1,1 +1,1 @@
-hexo clean && hexo g && git add . &&  git commit -m "win git commit blog" &&  git push -u origin main -f 
+hexo clean && hexo g && hexo d && git add . &&  git commit -m "win git commit blog" &&  git push -u origin main -f 
